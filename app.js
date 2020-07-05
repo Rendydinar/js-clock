5c04f51ed1d63eff05269d2d58ac71b7da3d3b97
R3ndydinar <Genjitapaleuk@gmail.com>
2020-07-05 14:27:13 +0700

    Mendukung PWA

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Clock
 const deg = 6;
 const hr = document.getElementById('hr');
 const mn = document.getElementById('mn');
@@ -14,3 +15,30 @@ setInterval(function(){
 	mn.style.transform = `rotateZ(${mm}deg)`;
 	sc.style.transform = `rotateZ(${ss}deg)`;
 }, 1000); 
+
+// Mode Handle
+const darkToggle = document.getElementById('toggle-dark-mode');
+const ligthToggle = document.getElementById('toggle-ligth-mode');
+darkToggle.addEventListener('click', function() {
+  document.body.style.background = '#091921';
+})
+
+ligthToggle.addEventListener('click', function() {
+  document.body.style.background = '#ffffff';
+})
+
+
+// Mendaftarkan service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+      // Registration was successful
+      // Berhasil mendaftarkan Service worker di halaman situs
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      // Gagal mendaftarkan Service worker di halaman situs
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
